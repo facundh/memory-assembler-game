0576b07e68765d55d4dc29043df70ddbda951fb9
Facundo Herrera <facundhfed@gmail.com>
2023-04-27 19:28:21 +0200

    Userform and startform checked, information in localStorage ok

--- form.js
+++ form.js
@@ -3,58 +3,56 @@ const userForm = document.createElement("form");
 let titleForm = document.createElement("label");
 const userName = document.createElement("input");
 const buttonGame = document.createElement("input");
+gameContainer.classList.remove("gallery");
+
 
 // Atributes Elements
 userForm.setAttribute("method", "post");
 userForm.setAttribute("action", "");
 userForm.setAttribute("class", "form-style")
-
 titleForm.setAttribute("class", "title-form")
-
 userName.setAttribute("type", "text");
 userName.setAttribute("placeholder", "Choose your Username");
 userName.setAttribute("class", "user-input");
 userName.setAttribute("id", "userInput");
-
-
 buttonGame.setAttribute("type", "button");
 buttonGame.setAttribute("value", "Continue");
-buttonGame.setAttribute("onclick", "console.log(userName.value)");
 buttonGame.setAttribute("class", "btn-user");
-
-//Error Message
 
 
 // Player
-let player = {
-    playerName:"",
-    score:0
-};
-
-userName.addEventListener("input", () => {
-    player.name = userName.value;
-
-});
-
-const { playerName, score } = player;
-
-buttonGame.addEventListener('click', () => {
-    if(playerName == "" ) {
-        userName.setAttribute("class", "required-input");
-        userName.setAttribute("placeholder", "Username is required");
-        return
+userName.addEventListener("input", function(){
+    let playerName = userName.value;
+    if(playerName.length == 0 || playerName.length < 4 || playerName.currentplayer == ""){
+        userName.classList.add("required-input");
     } else {
-        playerName = userName.value;
-        score = 0;
-        console.log({player})
+        userName.classList.remove("required-input");
+        userName.classList.add("user-input");
     }
-   
 });
 
-titleForm.innerHTML = "Welcome to the Memory Game";
+function saveLS(){
+    let currentUser = {player : userName.value, score: 0};
+    if(localStorage.getItem("players") === null){
+        localStorage.setItem("players", `[${JSON.stringify(currentUser)}]`);
+    } else {
+        let playersArr = Array.from(JSON.parse(localStorage.getItem("players")));
+        playersArr.push(currentUser);
+        localStorage.setItem("players", JSON.stringify(playersArr));
+    }
+}
+
+buttonGame.addEventListener("click", function(){
+    saveLS();
+    container.classList.add("no-show");
+    startGameContainer.classList.remove("no-show");
+    startGameContainer.classList.add("show");
+    userName.value = "";
+});
+
+titleForm.textContent = "Welcome to the Memory Game";
 userForm.appendChild(titleForm);
 userForm.appendChild(userName);
 userForm.appendChild(buttonGame);
-
 container.appendChild(userForm);
 
--- startGame.js
+++ startGame.js
@@ -3,12 +3,25 @@ const boxStartGame = document.createElement("form");
 const titleFormStart = document.createElement("label");
 const btnStartGame = document.createElement("input");
 
-boxStartGame.setAttribute("class", "start-form-style");
+boxStartGame.setAttribute("class", "show");
 titleFormStart.setAttribute("class", "start-title-form");
 btnStartGame.setAttribute("class", "btn-user");
 btnStartGame.setAttribute("value", "Start Game");
+btnStartGame.setAttribute("type", "button");
 
-titleFormStart.innerHTML = "Are you ready?"
+
+
+// Take data from LS
+// let profile = JSON.parse(localStorage.getItem("player"));
+// console.log(profile)
+titleFormStart.innerHTML = `Are you ready ?`
 boxStartGame.appendChild(titleFormStart);
 boxStartGame.appendChild(btnStartGame);
 startGameContainer.appendChild(boxStartGame);
+
+
+btnStartGame.addEventListener("click", () => {
+    startGameContainer.classList.add("no-show");
+    startGameContainer.classList.remove("show");
+    gameContainer.classList.remove("no-show")
+});
